af22e7e66ca06cfe6d076900d507b3e31547476a
Juliette Le Goff <juliettelg30@gmail.com>
2024-07-25 12:15:16 +0100

    line indicating direction working properly

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rafasant <rafasant@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: jle-goff <jle-goff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 17:06:58 by jle-goff          #+#    #+#             */
-/*   Updated: 2024/07/24 17:30:46 by rafasant         ###   ########.fr       */
+/*   Updated: 2024/07/25 12:14:17 by jle-goff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,32 +30,62 @@ void	my_mlx_pixel_put(t_img *data, int x, int y, int colour)
 	*(unsigned int*)dst = colour;
 }
 
-void	draw_line(t_img	*img, int x, int y, int colour, t_player *player)
-{
-	int	i;
-	int	j;
+// void	draw_line(t_img	*img, int x, int y, int colour, t_player *player)
+// {
+// 	int	i;
+// 	int	j;
 
-	i = x;
-	j = y;
-	printf("a\n");
-	printf("dirY %f\n", player->dirY);
-	printf("dirX %f\n", player->dirX);
-	printf("i %i", i);
-	printf("j %i", j);
-	while (i < (x * SIZE) + SIZE && i > 0)
-	{
-		j = y;
-		while (j < (y * SIZE) + SIZE && j > 0)
-		{
-			my_mlx_pixel_put(img, i, j, colour);
-			j++;
-			j *= player->dirY;
-		}
-		printf("in here\n");
-		i++;
-		i *= player->dirX;
-		printf("i is now %i", i);
-	}
+// 	i = x;
+// 	j = y;
+// 	printf("a\n");
+// 	printf("dirY %f\n", player->dirY);
+// 	printf("dirX %f\n", player->dirX);
+// 	printf("i %i", i);
+// 	printf("j %i", j);
+// 	while (i < (x * SIZE) + SIZE && i > 0)
+// 	{
+// 		j = y;
+// 		while (j < (y * SIZE) + SIZE && j > 0)
+// 		{
+// 			my_mlx_pixel_put(img, i, j, colour);
+// 			j++;
+// 			j *= player->dirY;
+// 		}
+// 		printf("in here\n");
+// 		i++;
+// 		i *= player->dirX;
+// 		printf("i is now %i", i);
+// 	}
+// }
+
+
+void
+plot_line (int x0, int y0, int x1, int y1, t_game *game, int colour)
+{
+  int dx =  abs (x1 - x0), sx = x0 < x1 ? 1 : -1;
+  int dy = -abs (y1 - y0), sy = y0 < y1 ? 1 : -1; 
+  int err = dx + dy, e2; /* error value e_xy */
+ 
+  for (;;){  /* loop */
+    mlx_pixel_put(game->win->mlx_ptr, game->win->win_ptr, x0,y0, colour);
+    if (x0 == x1 && y0 == y1) break;
+    e2 = 2 * err;
+    if (e2 >= dy) { err += dy; x0 += sx; } /* e_xy+e_x > 0 */
+    if (e2 <= dx) { err += dx; y0 += sy; } /* e_xy+e_y < 0 */
+  }
+}
+
+void	draw_line(t_game *game, int colour)
+{
+	int size_offset_posX = round(game->player->posX * SIZE + SIZE/2);
+	int size_offset_posY = round(game->player->posY * SIZE + SIZE/2);
+	int	size_offset_pos2X = round(size_offset_posX + (SIZE * game->player->dirX));
+	int	size_offset_pos2Y = round(size_offset_posY + (SIZE * game->player->dirY));
+
+	printf("Center: (%d, %d)\n", size_offset_posX, size_offset_posY);
+	printf("End: (%d, %d)\n", size_offset_pos2X, size_offset_pos2Y);
+	
+	plot_line(size_offset_posX, size_offset_posY, size_offset_pos2X, size_offset_pos2Y, game, colour);
 }
 
 //draws square image
@@ -86,29 +116,30 @@ int key_press(int keycode, void *param)
 	int		map_ipos;
 	int		map_jpos;
 
-	printf("%i\n", keycode);
 	game = (t_game *)param;
+
+	draw_line(game, 0x00000000);
 	mlx_put_image_to_window(game->win->mlx_ptr, game->win->win_ptr, game->bgd->img_ptr,
 		game->player->posX * SIZE, game->player->posY * SIZE);
     if (keycode == S)//&& game->map[game->player->posY][game->player->posX + 1] != '1')
 	{
-		game->player->posX += game->player->dirX;
-		game->player->posY += game->player->dirY;
-	}
-	if (keycode == W)//&& game->map[game->player->posY][game->player->posX - 1] != '1')
-	{
 		game->player->posX -= game->player->dirX;
 		game->player->posY -= game->player->dirY;
 	}
+	if (keycode == W)//&& game->map[game->player->posY][game->player->posX - 1] != '1')
+	{
+		game->player->posX += game->player->dirX;
+		game->player->posY += game->player->dirY;
+	}
 	if (keycode == D) // Strafe Right aka find perpendicular vector with 90 degree clockwise rotation
 	{
-		game->player->posX += game->player->dirY; // Move rightward along the X axis
-		game->player->posY -= game->player->dirX; // Move rightward along the Y axis
+		game->player->posX -= game->player->dirY; // Move rightward along the X axis
+		game->player->posY += game->player->dirX; // Move rightward along the Y axis
 	}
 	if (keycode == A) // Strafe Left aka find perpendicular vector with 90 degree anti-clockwise rotation
 	{
-		game->player->posX -= game->player->dirY; // Move leftward along the X axis
-		game->player->posY += game->player->dirX; // Move leftward along the Y axis
+		game->player->posX += game->player->dirY; // Move leftward along the X axis
+		game->player->posY -= game->player->dirX; // Move leftward along the Y axis
 	}
 	if (keycode == LEFT) {
         double oldDirX = game->player->dirX;
@@ -126,7 +157,9 @@ int key_press(int keycode, void *param)
         game->player->planeX = game->player->planeX * cos(ROT_SPEED) -  game->player->planeY * sin(ROT_SPEED);
          game->player->planeY = oldPlaneX * sin(ROT_SPEED) +  game->player->planeY * cos(ROT_SPEED);
     }
-
+	
+	draw_line(game, 0x00FF0000);
+	
 	mlx_put_image_to_window(game->win->mlx_ptr, game->win->win_ptr, game->player->img->img_ptr,
 		game->player->posX * SIZE, game->player->posY * SIZE);
     return (0);
@@ -181,12 +214,12 @@ int	main(void)
 				if (game.map[i][j] == 'N')
 				{
 					player.dirX = 0;
-					player.dirY = 1;
+					player.dirY = -1;
 				}
 				if (game.map[i][j] == 'S')
 				{
 					player.dirX = 0;
-					player.dirY = -1;
+					player.dirY = 1;
 				}
 				if (game.map[i][j] == 'W')
 				{
@@ -203,7 +236,9 @@ int	main(void)
 		}
 		i++;
 	}
-	draw_line(&dir_line, player.posX, player.posY, 0x00FF0000, &player);
+	
+	draw_line(&game, 0x00FF0000);
+	
 	mlx_put_image_to_window(win.mlx_ptr, win.win_ptr, dir_line.img_ptr, player.posX * SIZE + SIZE / 2, player.posY * SIZE - SIZE / 2);
 	player.planeX = 0;
 	player.planeY = 0.66;
